4480e2fa20dee789d4349186b76a0793e083d4d4
VirusCamp <viruscamp@gmail.com>
2024-03-16 16:31:25 +0800

    gtk_header_bar

--- gtk-samp1-c/main.c
+++ gtk-samp1-c/main.c
@@ -1,23 +1,44 @@
 #include <gtk/gtk.h>
 
+#ifndef USE_DEFAULT_TITLE_BAR
+#define USE_DEFAULT_TITLE_BAR 0
+#endif
+
 static void
-activate (GtkApplication* app,
+activate (GtkApplication *app,
           gpointer        user_data)
 {
     GtkWidget *window = gtk_application_window_new (app);
 
-    GdkDisplay* display = gdk_display_get_default();
-    const gchar* display_type = g_type_name(gdk_display_get_type());
+    GdkDisplay *display = gdk_display_get_default();
+    const gchar *display_type = g_type_name(gdk_display_get_type());
 
-    const gchar* display_name = gdk_display_get_name(display);
-    GString* title = g_string_new("GTK Sample 1 @");
+    const gchar *display_name = gdk_display_get_name(display);
+    GString *title = g_string_new("GTK Sample 1 @");
     g_string_append(title, display_name);
     //g_string_append(title, " ");
     //g_string_append(title, display_type);
 
-    gtk_window_set_title (GTK_WINDOW (window), g_string_free_and_steal(title));
+    if (USE_DEFAULT_TITLE_BAR) {
+        gtk_window_set_title (GTK_WINDOW (window), g_string_free_and_steal(title));
+    } else {
+        GtkWidget *header_bar = gtk_header_bar_new();
+        gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header_bar), TRUE);
+        gtk_header_bar_set_title(GTK_HEADER_BAR (header_bar), g_string_free_and_steal(title));
+
+        GtkWidget *btn_head1 = gtk_button_new();
+        GtkWidget *btn_head1_label = gtk_label_new_with_mnemonic("_Hello");
+        gtk_container_add (GTK_CONTAINER (btn_head1), btn_head1_label);
+        gtk_header_bar_pack_start(GTK_HEADER_BAR (header_bar), btn_head1);
+
+        gtk_window_set_titlebar(GTK_WINDOW (window), header_bar);
+    }
+
+    gtk_container_add (GTK_CONTAINER (window), gtk_text_view_new());
+
     gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
     gtk_window_present (GTK_WINDOW (window));
+    gtk_widget_show_all(window);
 }
 
 int
--- gtk-samp2-cpp/main.cpp
+++ gtk-samp2-cpp/main.cpp
@@ -1,19 +1,44 @@
 #include <gtkmm.h>
 
+#ifndef USE_DEFAULT_TITLE_BAR
+#define USE_DEFAULT_TITLE_BAR 0
+#endif
+
 class MyWindow : public Gtk::Window
 {
+private:
+  Gtk::HeaderBar header;
+  Gtk::Button btn_head1;
+  Gtk::Label btn_head1_label;
+  Gtk::TextView text_view;
 public:
   MyWindow();
 };
 
 MyWindow::MyWindow()
 {
-    auto display = Gdk::Display::get_default();
-    auto display_name = display->get_name();
-    Glib::ustring title = "GTK Sample 2 @";
-    title.append(display_name);
+  auto display = Gdk::Display::get_default();
+  auto display_name = display->get_name();
+  Glib::ustring title = "GTK Sample 2 @";
+  title.append(display_name);
+
+  if (USE_DEFAULT_TITLE_BAR) {
     set_title(title);
-    set_default_size(400, 400);
+  } else {
+    btn_head1_label.set_text_with_mnemonic("_Hello");
+    btn_head1.add(btn_head1_label);
+
+    header.set_show_close_button(true);
+    header.set_title(title);
+    header.pack_start(btn_head1);
+
+    set_titlebar(header);
+  }
+  set_default_size(400, 400);
+
+  add(text_view);
+
+  show_all();
 }
 
 int main(int argc, char* argv[])
